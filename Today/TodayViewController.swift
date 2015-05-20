3d1f54bc920a933428cfcfdc89fb716a42439d7b
Jeong YunWon <jeong@youknowone.org>
2015-05-21 08:29:40 +0900

    Today widget

---
+++ Today/TodayViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TodayViewController.swift
+//  Today
+//
+//  Created by Jeong YunWon on 2015. 5. 21..
+//  Copyright (c) 2015년 youknowone.org. All rights reserved.
+//
+
+import Cocoa
+import NotificationCenter
+//import SayKit
+
+class TodayViewController: NSViewController, NCWidgetProviding, NSTextFieldDelegate {
+
+    override var nibName: String? {
+        return "TodayViewController"
+    }
+
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Update your data and prepare for a snapshot. Call completion handler when you are done
+        // with NoData if nothing has changed or NewData if there is new data since the last
+        // time we called you
+        completionHandler(.NoData)
+    }
+
+    func control(control: NSControl, textView: NSTextView, doCommandBySelector commandSelector: Selector) -> Bool {
+        if commandSelector == "insertNewline:" {
+            let say = SKSay(text: textView.string ?? "")
+            say.play(false)
+        }
+        return false
+    }
+}
