46e42c67d7fd2949eaef392a0878889f498b5b62
Jeong YunWon <jeong@youknowone.org>
2016-09-24 14:36:09 +0900

    iOS base

---
+++ iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Jeong YunWon on 2016. 9. 24..
+//  Copyright © 2016년 youknowone.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
