bf2529cbb45a62ffaef7c46e563822c5ff7b9769
Seungwon Kim <closer527@gmail.com>
2016-09-26 18:53:46 +0900

    add comments for documentation

--- Say/Say.swift
+++ Say/Say.swift
@@ -8,6 +8,9 @@
 
 import Cocoa
 
+/**
+    Easy-to-use interface for `say` command in OS X.
+*/
 class SayAPI: NSObject {
     let speechSynthesizer: NSSpeechSynthesizer?
     var text: String
@@ -21,6 +24,13 @@ class SayAPI: NSObject {
         }
     }
 
+    /**
+        Construct a say interface with given text and voice.
+     
+        - Parameters:
+        - text: A text string to composite speech.
+        - voice: A voice to composite speech. If given voice is nil, default voice is used.
+     */
     init(text: String, voice: VoiceAPI?) {
         speechSynthesizer = NSSpeechSynthesizer.init()
         self.text = text
@@ -28,19 +38,44 @@ class SayAPI: NSObject {
         super.init()
     }
     
+    /**
+        Construct a say interface with given text and default voice.
+     
+        - Parameter text: A text string to composite speech.
+     */
     public convenience init(text: String) {
         self.init(text: text, voice: nil)
     }
     
+    /**
+        Construct a say interface with given text and voice with given voice name.
+     
+        - Parameter text: A text string to composite speech.
+        - Parameter voiceIdentifier: A voice identifier to composite speech. If given voice name is invalid, nil is returned.
+    */
     public convenience init?(text: String, voiceIdentifier: String) {
         self.init(text: text, voice: VoiceAPI.init(dictionary: NSSpeechSynthesizer.attributes(forVoice: voiceIdentifier)))
     }
 
+    /**
+        Composite and play speech.
+     
+        - Parameter waitUntilDone: Currently ignored.
+     */
     func play(_ waitUntilDone: Bool) {
         speechSynthesizer?.setVoice(self.voice?.identifier)
         speechSynthesizer?.startSpeaking(text);
     }
     
+    /**
+        Composite and write speech to URL.
+     
+        File format is .aiff.
+     
+        - Parameters:
+        - URL: File URL to write speech.
+        - atomically: Currently ignored.
+     */
     func writeToURL(_ URL: Foundation.URL, atomically: Bool) {
         speechSynthesizer?.setVoice(self.voice?.identifier)
         speechSynthesizer?.startSpeaking(text, to: URL)
