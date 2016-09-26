f306a019309f5f9a7a1dc7c6dde06ea9b83e87f8
Lee GyuJin <amanida@stone-i.co.kr>
2016-09-26 18:14:14 +0900

    iOS base UI and functions (#23)
    * added base UI for functional test
    
    * porting speaker.swift for swift 3
    
    * Added playing function.
    
    * Added action sheet with picker View. (For language selecting feature)
    Added changeLanguage method in speaker.
    
    * Completed voice change feature.
    Created voices property for changing voice feature.
    
    * Solved picker view layout problem. (reviewed by youknowone)
    Added upside Y margin to picker view and extended height.
    
    * Changed default speaker language.
    
    * Changed speaker utterance rate default.
    
    * Changed alertView to alertController.

---
+++ iOS/Speaker.swift
@@ -0,0 +1,57 @@
+//
+//  Speaker.swift
+//  BotTalk
+//
+//  Created by Jeong YunWon on 2016. 7. 7..
+//  Copyright © 2016년 youknowone.org. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+public class Speaker: NSObject, AVSpeechSynthesizerDelegate {
+    static let defaultSpeaker = Speaker()
+    var volume: Float = 1.0
+    var mute = false
+    var voice = AVSpeechSynthesisVoice(language: "en-US")     // set default language : english (temporarily)
+    var voices = AVSpeechSynthesisVoice.speechVoices()
+    
+    let synthesizer = AVSpeechSynthesizer()
+
+    override init() {
+        super.init()
+        self.synthesizer.delegate = self
+    }
+
+    public func stopSpeaking() {
+        self.synthesizer.stopSpeaking(at: .word)
+    }
+
+    public func pauseSpeaking() {
+        self.synthesizer.pauseSpeaking(at: .immediate)
+    }
+
+    public func continueSpeaking() {
+        self.synthesizer.continueSpeaking()
+    }
+
+    public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+//        Do what speeching finished
+        print("speeching finished")
+    }
+
+    public func speakText(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.volume = self.mute ? 0.0 : self.volume
+        utterance.voice = self.voice
+        self.synthesizer.speak(utterance)
+    }
+    
+    public func changeLanguage(language: String) {
+        self.voice = AVSpeechSynthesisVoice(language: language)
+    }
+}
+
+protocol SpeakerDelegate {
+    func speaker(speaker: Speaker, didFinishSpeechString: String);
+}
--- iOS/ViewController.swift
+++ iOS/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SpeakerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet var textview: UITextView!
+    var languagePicker: UIPickerView = UIPickerView()
+    let speaker = Speaker.defaultSpeaker
+    var languageNames = [String]()
+    
+    @IBAction func playClicked(_ sender: AnyObject) {
+        if let text = self.textview.text {
+            self.speaker.speakText(text: text)
+        }
+    }
+    @IBAction func changeVoiceClicked(_ sender: AnyObject) {
+        // create action sheet dynamically
+        let alertController = UIAlertController(title: "Select Launguage", message: "\n\n\n\n\n\n\n\n\n\n", preferredStyle: UIAlertControllerStyle.actionSheet);
+        self.languagePicker.center.x = self.view.center.x
+        let action = UIAlertAction(title: "DONE", style: .default) { (action) in
+            let value = self.languagePicker.selectedRow(inComponent: 0)
+            self.speaker.changeLanguage(language: self.speaker.voices[value].language)
+        }
+        alertController.addAction(action)
+        alertController.view.addSubview(languagePicker)
+        present(alertController, animated: true, completion: nil)
+        self.languagePicker.autoresizingMask = [UIViewAutoresizing.flexibleLeftMargin, UIViewAutoresizing.flexibleRightMargin]
+    }
+    
+    func speaker(speaker: Speaker, didFinishSpeechString: String) {
+        print("didFinishSpeechString : ", didFinishSpeechString)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // create popup picker view
+        languagePicker = UIPickerView(frame: CGRect(x: 0, y: 20, width: 300, height: 250))
+        languagePicker.delegate = self
+        languagePicker.dataSource = self
+        languagePicker.showsSelectionIndicator = true
+        languagePicker.tintColor = UIColor.red
+        languagePicker.reloadAllComponents()
+        
+        //init language names
+        let voices = self.speaker.voices
+        for voice in voices {
+            languageNames.append(voice.name)
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.speaker.voices.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let name = self.languageNames[row]
+        let languageCode = self.speaker.voices[row].language
+        return "\(name)(\(languageCode))"
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
